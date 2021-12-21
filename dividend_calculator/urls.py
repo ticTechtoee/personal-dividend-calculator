37e07f5f71f6842b5538c4da4e2588ee98b8241d
Farhan Basheer <farhanfb6@outlook.com>
2021-12-21 21:33:16 +0500

    second commit

--- dividend_calculator/urls.py
+++ dividend_calculator/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.urls.conf import include
+from django.conf.urls.static import static
 
 from django.views.static import serve
 from django.conf.urls import url
@@ -12,3 +13,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
