23002e3dcfb9f51ed6c7d5c1502a9e2f632eda19
Farhan Basheer <farhanfb6@outlook.com>
2021-12-21 21:13:55 +0500

    first commit

---
+++ dividend_calculator/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.urls.conf import include
+
+from django.views.static import serve
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls'), name='home'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
